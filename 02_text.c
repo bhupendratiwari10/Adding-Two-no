fe0b3b9d3b61ef6d6a9167f7bc872e5bc10da1e3
bhupendratiwari10 <bhupendratiwari675@gmail.com>
2021-01-04 18:51:54 +0530

    hello

---
+++ 02_text.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    float b;
+    printf("Enter the value of a");
+scanf("%d",&a);
+    printf("Enter the value of b");
+    scanf("%f",&b);
+printf("sum of %d and %f is %f\n",a + b );
+    return 0;
+}
